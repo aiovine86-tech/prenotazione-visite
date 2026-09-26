301752957ffc20ff27a5519842fe003db4a26e6d
aiovine86-tech <a.iovine86@gmail.com>
2026-09-26 13:40:01 +0200

    Create vicinanze.py

---
+++ vicinanze.py
@@ -0,0 +1,23 @@
+VICINANZE = {
+    "81030|LUSCIANO": {
+        "81031|AVERSA",
+        "81030|PARETE",
+        "81030|GRICIGNANO DI AVERSA",
+        "81030|SUCCIVO",
+        "81030|TEVEROLA",
+        "81030|SANT'ARPINO",
+    },
+
+    "81030|CASTEL VOLTURNO": {
+        "81037|BAIA DOMIZIA",
+        "81030|CELLOLE",
+    },
+
+    "81020|CAPODRISE": {
+        "81020|SAN MARCO EVANGELISTA",
+        "81100|CASERTA",
+        "81031|AVERSA",
+    },
+
+    # ...
+}
